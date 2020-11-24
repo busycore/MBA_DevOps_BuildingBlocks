5f4ecc9d06c34ad963f043b53234d1031f5d048b
Matheus V <matheusvrgm@gmail.com>
2020-11-24 01:54:50 -0300

    created a security problem

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("alos");
+import User from "./modules/User/entities/User.entity";
+
+const user = new User("Joao", "213");
+
+console.log(user.generateWeakPass());
--- src/modules/User/entities/User.entity.ts
+++ src/modules/User/entities/User.entity.ts
@@ -1,3 +1,5 @@
+import crypto from "crypto";
+
 export default class User {
   constructor(name: string, password: string) {
     this.name = name;
@@ -9,5 +11,15 @@ export default class User {
 
   public authenticate(pass: string): boolean {
     return pass === this.password;
+  }
+
+  public generateWeakPass(): any {
+    let iv = crypto.randomBytes(16);
+    let cipher = crypto.createCipheriv(
+      "aes-256-cbc",
+      Buffer.from("11111111111111111111111111111111"),
+      iv
+    );
+    return cipher; // Noncompliant: DES / 3DES is unsecure Noncompliant: DES / 3DES is unsecure
   }
 }
