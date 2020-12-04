188f88d70b43ce9c7b037a9b19bc7519479aecf1
Matheus V <matheusvrgm@gmail.com>
2020-12-04 11:36:58 -0300

    started a route for testing

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,38 @@
-import { exec } from "child_process";
 import User from "./modules/User/entities/User.entity";
+import cors from "cors";
+import express, { NextFunction, Response, Request } from "express";
 
-console.log("Starting the program");
+// respond with "hello world" when a GET request is made to the homepage
+const appInsights = require("applicationinsights");
+
+appInsights
+  .setup("ab27de42-b07f-4752-a8e9-0c20ea37ff90")
+  .setAutoDependencyCorrelation(true)
+  .setAutoCollectRequests(true)
+  .setAutoCollectPerformance(true, true)
+  .setAutoCollectExceptions(true)
+  .setAutoCollectDependencies(true)
+  .setAutoCollectConsole(true)
+  .setUseDiskRetryCaching(true)
+  .setSendLiveMetrics(false)
+  .setDistributedTracingMode(appInsights.DistributedTracingModes.AI)
+  .start();
+const app = express();
+app.use(cors());
 const user = new User("Joao", "213");
 
-console.log(user.generateWeakPass());
+app.listen(3333, () => {
+  console.log("[ 🟢 ] Server started");
+  appInsights.defaultClient.trackEvent({
+    name: "Server started",
+    properties: { startedTime: new Date() },
+  });
+});
 
-exec("cat");
-
-exec("http://localhost:80/");
-
-exec("http://www.google.com:80/");
+app.get("/", function (req, res) {
+  appInsights.defaultClient.trackRequest({
+    name: "GET /",
+    url: req.url,
+  });
+  res.send("<b>Hello World</b>");
+});
